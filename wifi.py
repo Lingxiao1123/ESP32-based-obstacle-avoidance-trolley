ed5cf0fe026b7068a3bfef6a185218813dcd0fd1
LingxiaoPan <118569109+Lingxiao1123@users.noreply.github.com>
2023-04-25 14:49:25 -0700

    Add files via upload

--- wifi.py
+++ wifi.py
@@ -1,15 +1,18 @@
 import network
 import time
+from time import sleep
 import wheelmotor
 from machine import Pin
 import socket
+import ultra_Action
 
 #配置网络模块
 net = network.WLAN(network.STA_IF)
 if not net.isconnected():
     net.active(True)
     #连接无线网络，设立wifi id 和密码
-    net.connect('ATT 201','ibib776j+mx8')
+#     net.connect('ATT 201','ibib776j+mx8')
+    net.connect('Berkeley-IoT','fc!N7$27')
 print(net.ifconfig())
 
 html = """<!DOCTYPE html>
@@ -35,33 +38,38 @@ s.bind(('0.0.0.0','80'))
 #listen 80 port
 s.listen(1)
 
-State = "Initialization"
-
-while True:
-    #socket阻塞外部连接
-    cl,addr = s.accept()
-    #拿到客户端IP地址
-    print('Client connected from',addr)
-    #拦截客户端请求
-    request = cl.recv(1024)
-    if request.decode()[:100].find("Front") != -1:
-        wheelmotor.moveFront()
-        State = "move front"
-    elif request.decode()[:100].find("Back") != -1:
-        wheelmotor.moveBack()
-        State = "move back"
-    elif request.decode()[:100].find("Left") != -1:
-        wheelmotor.turnLeft()
-        State = "turn left"
-    elif request.decode()[:100].find("Right") != -1:
-        wheelmotor.turnRight()
-        State = "turn right"
-    elif request.decode()[:100].find("STOP") != -1:
-        wheelmotor.stop()
-        State = "Stop"
-    response = html%State
-    cl.send(response)
-    cl.close()
+def connectWeb():
+    State = "Initialization"
+    while True:
+        #socket阻塞外部连接
+        cl,addr = s.accept()
+        #拿到客户端IP地址
+        print('Client connected from',addr)
+        #拦截客户端请求
+        request = cl.recv(1024)
+        if request.decode()[:100].find("Front") != -1:
+            wheelmotor.moveFront()
+            State = "move front"
+        elif request.decode()[:100].find("Back") != -1:
+            wheelmotor.moveBack()
+            State = "move back"
+        elif request.decode()[:100].find("Left") != -1:
+            wheelmotor.turnLeft()
+            State = "turn left"
+            sleep(1.5)
+            wheelmotor.stop()
+        elif request.decode()[:100].find("Right") != -1:
+            wheelmotor.turnRight()
+            State = "turn right"
+            sleep(1.5)
+            wheelmotor.stop()
+        elif request.decode()[:100].find("STOP") != -1:
+            wheelmotor.stop()
+            State = "Stop"
+        response = html%State
+        cl.send(response)
+        cl.close()
+          
     
         
     
